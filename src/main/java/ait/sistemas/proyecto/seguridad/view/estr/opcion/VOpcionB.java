ee3030f5f4ac09c4bd3a69d1556f06567d67e8f9
franzemil <franzemil35@gmail.com>
2015-07-13 09:03:41 -0400

    Se realizo el programa de  Eliminar Opcion

--- src/main/java/ait/sistemas/proyecto/seguridad/view/estr/opcion/VOpcionB.java
+++ src/main/java/ait/sistemas/proyecto/seguridad/view/estr/opcion/VOpcionB.java
@@ -1,4 +1,4 @@
-package ait.sistemas.proyecto.seguridad.view.estr.submenu;
+package ait.sistemas.proyecto.seguridad.view.estr.opcion;
 
 import java.util.List;
 
@@ -28,33 +28,36 @@ import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 
-@CDIView(value=VSubMenuB.ID)
-public class VSubMenuB extends VerticalLayout implements View, ClickListener, SelectionListener,org.vaadin.dialogs.ConfirmDialog.Listener{
- 
+@CDIView(value=VOpcionB.ID)
+public class VOpcionB extends VerticalLayout implements View, ClickListener, SelectionListener,org.vaadin.dialogs.ConfirmDialog.Listener{
+
 	private static final long serialVersionUID = 1L;
-	public static final String ID = "/seg/estr/submenu/b";
+	public static final String ID = "/seg/estr/opcion/b";
 	
 	private Button btn_submit;
 	private Button btn_limpiar;
-	private FormSubMenu frm_subMenu;
-	private GridSubMenu grid_subMenu;
+	private FormOpcion frm_opcion;
+	private GridOpcion grid_opcion;
 	private CssLayout hl_errores;
 	private MenuImpl menu;
-	
-	public VSubMenuB() {
+	public VOpcionB() {
 		
-		this.frm_subMenu = new FormSubMenu();
+		this.frm_opcion = new FormOpcion();
 		this.menu = new MenuImpl();
 		this.btn_submit = new Button("Eliminar");
 		this.btn_limpiar = new Button("Limpiar");
 		this.hl_errores = new CssLayout();
-		this.grid_subMenu = new GridSubMenu();
-		this.grid_subMenu.addSelectionListener(this);
-		this.frm_subMenu.cb_icons.setEnabled(false);
-		this.frm_subMenu.cbMenus.setEnabled(false);
-		this.frm_subMenu.cbSubsistema.setEnabled(false);
-		this.frm_subMenu.txt_nombre_menu.setEnabled(false);
-		this.frm_subMenu.txt_nombre_programa.setEnabled(false);
+		this.grid_opcion = new GridOpcion();
+		this.grid_opcion.addSelectionListener(this);
+		
+		
+		this.frm_opcion.cb_icons.setEnabled(false);
+		this.frm_opcion.cbMenus.setEnabled(false);
+		this.frm_opcion.cbSubsistema.setEnabled(false);
+		this.frm_opcion.cbSubMenus.setEnabled(false);
+		this.frm_opcion.txt_nombre_menu.setEnabled(false);
+		this.frm_opcion.txt_nombre_programa.setEnabled(false);
+			
 		addComponent(buildNavBar());
 		addComponent(buildFormContent());
 		addComponent(buildButtonBar());
@@ -65,14 +68,15 @@ public class VSubMenuB extends VerticalLayout implements View, ClickListener, Se
 		formContent.setSpacing(true	);
 		Panel frmPanel = new Panel();
 		frmPanel.setWidth("100%");
-		frmPanel.setCaption("Formulario Menu");
-		frmPanel.setContent(this.frm_subMenu);
+		frmPanel.setCaption("Formulario Opcion");
+		this.frm_opcion.init();
+		frmPanel.setContent(this.frm_opcion);
 		formContent.setMargin(true);
 		formContent.addComponent(frmPanel);
 		Panel gridPanel = new Panel();
 		gridPanel.setWidth("100%");
-		gridPanel.setCaption("GRid Menu");
-		gridPanel.setContent(this.grid_subMenu);
+		gridPanel.setCaption("Grid Opcion");
+		gridPanel.setContent(this.grid_opcion);
 		formContent.setMargin(true);
 		formContent.addComponent(frmPanel);
 		formContent.addComponent(gridPanel);
@@ -86,8 +90,8 @@ public class VSubMenuB extends VerticalLayout implements View, ClickListener, Se
 		nav.addStyleName("ait-content-nav");
 		nav.addComponent(new Label("Seguridad » "));
 		nav.addComponent(new Label("Estructura del Sistema » "));
-		nav.addComponent(new Label("Sun-Menu » "));
-		nav.addComponent(new Label("<strong>Modificar</strong>", ContentMode.HTML));
+		nav.addComponent(new Label("Opcion » "));
+		nav.addComponent(new Label("<strong>Eliminar</strong>", ContentMode.HTML));
 		navPanel.setContent(nav);
 		return navPanel;
 	}
@@ -105,51 +109,37 @@ public class VSubMenuB extends VerticalLayout implements View, ClickListener, Se
 	}
 	@Override
 	public void enter(ViewChangeEvent event) {
-		// TODO Auto-generated method stub
 		
 	}
+	
 	private void buildMessages(List<BarMessage> mensages) {
 		this.hl_errores.removeAllComponents();
 		hl_errores.addStyleName("ait-error-bar");
 		this.addComponent(this.hl_errores);
 		
 		for (BarMessage barMessage : mensages) {
-			Label lbError = new Label(new Label(barMessage.getComponetName()+":"+barMessage.getErrorName()));
+			Label lbError = new Label(new Label(barMessage.getComponetName()+" : "+barMessage.getErrorName()));
 			lbError.setStyleName(barMessage.getType());
 			this.hl_errores.addComponent(lbError);
 		}
-	
 	}
 	
 	@Override
 	public void buttonClick(ClickEvent event) {
 		if (event.getButton() == this.btn_submit) {
-			ConfirmDialog.show(getUI(), ("Eliminar " + this.frm_subMenu.getData().getAME_Nombre()),
-					("Esta Uds Seguro que Desea Eliminar el Sub-Sistema" + this.frm_subMenu.getData().getAME_Nombre()),
+			ConfirmDialog.show(getUI(), ("Eliminar " + this.frm_opcion.getData().getAME_Nombre()),
+					("Esta Uds Seguro que Desea Eliminar el Sub-Sistema" + this.frm_opcion.getData().getAME_Nombre()),
 					"Si", "No", this);	
 		}
 		if (event.getButton() == this.btn_limpiar) {
-			this.frm_subMenu.clearMessages();
+			this.frm_opcion.clearMessages();
 		}	
 	}
 	@Override
 	public void select(SelectionEvent event) {
-		if(this.grid_subMenu.getSelectedRow()!=null){
-		this.frm_subMenu.setData((Arbol_menus)this.grid_subMenu.getSelectedRow());
+		if((Arbol_menus)this.grid_opcion.getSelectedRow() != null){
+			this.frm_opcion.setData((Arbol_menus)this.grid_opcion.getSelectedRow());
 		}
-	}
-	private void eliminar() {
-		try {
-			this.menu.deleteSubsistema(this.frm_subMenu.getData().getAME_Id_Identificador());
-			Notification.show(Messages.SUCCESS_MESSAGE);
-			this.frm_subMenu.update();
-			this.grid_subMenu.update();
-			Notification.show(Messages.SUCCESS_MESSAGE);
-		} catch (Exception e) {
-			Notification.show(Messages.NOT_SUCCESS_MESSAGE, Type.ERROR_MESSAGE);
-		}buildMessages(this.frm_subMenu.getMensajes());
-		
-		this.frm_subMenu.clearMessages();
 	}
 	@Override
 	public void onClose(ConfirmDialog dialog) {
@@ -158,6 +148,18 @@ public class VSubMenuB extends VerticalLayout implements View, ClickListener, Se
 		}else{
 			
 		}
+	}
+	private void eliminar() {
+		try {
+			this.menu.deleteSubsistema(this.frm_opcion.getData().getAME_Id_Identificador());
+			Notification.show(Messages.SUCCESS_MESSAGE);
+			this.frm_opcion.update();
+			this.grid_opcion.update();
+			Notification.show(Messages.SUCCESS_MESSAGE);
+		} catch (Exception e) {
+			Notification.show(Messages.NOT_SUCCESS_MESSAGE, Type.ERROR_MESSAGE);
+		}buildMessages(this.frm_opcion.getMensajes());		
+		this.frm_opcion.clearMessages();
 	}
 
 }
