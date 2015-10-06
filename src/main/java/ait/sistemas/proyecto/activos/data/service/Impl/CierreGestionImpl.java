12124328c7087a5265822b0e0002a3891fc70db7
kimberly <kimx100pre@hotmail.es>
2015-10-06 09:39:04 -0400

    porgrama cierre de gestion

--- src/main/java/ait/sistemas/proyecto/activos/data/service/Impl/CierreGestionImpl.java
+++ src/main/java/ait/sistemas/proyecto/activos/data/service/Impl/CierreGestionImpl.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
-import javax.persistence.QueryHint;
 
 import org.eclipse.persistence.config.HintValues;
 import org.eclipse.persistence.config.QueryHints;
@@ -25,6 +24,19 @@ public class CierreGestionImpl {
 		query.setHint(QueryHints.REFRESH, HintValues.TRUE);
 		Cierre_Gestion resultlist = (Cierre_Gestion)query.getSingleResult();
 		return resultlist;
+	}
+	public int Cierre(Cierre_Gestion cierre_Gestion) {
+		Query query = em.createNativeQuery("EXEC Reva_CierreGestion_I "
+				+ "@Fecha=?1, "
+				+ "@Tipo_Cambio=?2 ");
+		query.setParameter(1,cierre_Gestion.getCGE_Fecha_Cierre_Gestion());
+		query.setParameter(2,cierre_Gestion.getCGE_Tipo_Cambio_UFV());
+		try {
+			int result = (Integer) query.getSingleResult();
+			return result;
+		} catch (Exception e) {
+			return 0;
+		}
 	}
 
 }
